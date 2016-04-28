52891df8c8fdb85940fc0af42fdb119485805051
John Smyth <john.smyth@workday.com>
2016-04-28 20:32:10 +0100

    Use ActiveModel serializer and adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import ENV from 'smart-client-app/config/environment';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.RESTAdapter.extend({
+export default ActiveModelAdapter.extend({
   host: ENV.host,
   pathForType: function(type) {
     return Ember.Inflector.inflector.pluralize(Ember.String.underscore(type));
